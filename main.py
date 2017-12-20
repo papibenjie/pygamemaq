ba0b426940e33f4d8e52b408593fdcca884eac2a
Marc-Antoine Quessy <papibenjie@gmail.com>
2017-12-19 19:27:12 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from pygamemaq.game import Game
+
+game = Game(600)
+
+print("akki")
+
+if 1:
+    pass
---
+++ pygamemaq/game/game.py
@@ -0,0 +1,5 @@
+class Game(object):
+    """docstring for Game."""
+    def __init__(self, arg):
+        super(Game, self).__init__()
+        self.arg = arg
